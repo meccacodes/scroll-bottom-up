f7943416a03ba03777d5a4c6d68b5e47348212c9
meccacodes <hi@windfallyarn.com>
2025-01-16 10:05:40 -0800

    Scroll Up From Bottom React Component

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ScrollToTop from "../components/ScrollToTop";
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <ScrollToTop />
+    </div>
+  );
+};
+
+export default Home;
---
+++ components/ScrollToTop.tsx
@@ -0,0 +1,36 @@
+"use client";
+import React, { useEffect, useRef } from "react";
+import styles from "./ScrollToTop.module.css";
+
+const ScrollToTop: React.FC = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const observer = new MutationObserver((mutations) => {
+      if (containerRef.current?.offsetHeight) {
+        window.scrollTo(0, document.body.scrollHeight);
+        observer.disconnect();
+      }
+    });
+
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
+
+    return () => observer.disconnect();
+  }, []);
+
+  return (
+    <div>
+      <div className={styles.contentWrapper} ref={containerRef}>
+        <div className={styles.sectionHeader}>
+          <h1>Scroll Up!</h1>
+          <p>^^^^^^</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ScrollToTop;
