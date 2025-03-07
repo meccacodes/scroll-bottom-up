c8f68c9c6f48aa8ebe54dadaf1729a4fb884f1b4
Mecca <hi@windfallyarn.com>
2025-03-07 18:18:18 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import styles from "./page.module.css";
 import ScrollToTop from "../components/ScrollToTop";
 
 const Home: React.FC = () => {
   return (
-    <div>
+    <div className={styles.pageWrapper}>
+      <div className={styles.party}>You made it!</div>
+      <div className={styles.party}>🥳</div>
       <ScrollToTop />
     </div>
   );
